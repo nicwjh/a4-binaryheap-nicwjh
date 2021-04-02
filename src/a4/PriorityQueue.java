1421a05b7d204d322d84cf18a4484fd43117d6bb
Nicholas <nicwjh@email.unc.edu>
2021-04-03 07:53:50 +0800

    build

--- src/a4/PriorityQueue.java
+++ src/a4/PriorityQueue.java
@@ -139,7 +139,7 @@ public class PriorityQueue implements  Queue{
     public void build(Prioritized[] elements) {
         int i = elements.length - 1;
 
-        while (i > 1) {
+        while (i > 0) {
             int swapIndex = i;
             if (i % 2 == 0) { //left leaf
                 if (i != elements.length - 1 && elements[i + 1].getPriority() > elements[i].getPriority()) {
